bf76a2697965e6a733d02cab7734fa12ba8c8b9c
khavyas <52441771+khavyas@users.noreply.github.com>
2025-08-24 17:57:55 +0530

    updated db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI); // USE the env variable
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI not defined in environment variables");
+    }
+
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB connected");
   } catch (err) {
     console.error("❌ MongoDB connection error:", err);
