5bf771c8f6bd7e3167e80395b06aa5dd03eaaa22
khavyas <52441771+khavyas@users.noreply.github.com>
2025-08-24 16:52:06 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,13 +2,10 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    console.log("MongoDB Connected");
-  } catch (error) {
-    console.error(error.message);
+    await mongoose.connect(process.env.MONGO_URI); // USE the env variable
+    console.log("✅ MongoDB connected");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err);
     process.exit(1);
   }
 };
