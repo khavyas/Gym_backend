840f64f7921f95c9498321d579e47d9183144b72
khavyas <52441771+khavyas@users.noreply.github.com>
2025-10-13 13:18:35 +0000

    port changes

--- server.js
+++ server.js
@@ -32,5 +32,5 @@ app.use("/api/consultants", consultantRoutes);
 app.use("/api/profile", profileRoutes);
 app.use('/api/gyms', gymRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
