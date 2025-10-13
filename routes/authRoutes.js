e1a83d56fbb7140bf6bdd82503f4a1fffc7dcd94
khavyas <52441771+khavyas@users.noreply.github.com>
2025-10-13 06:40:08 +0000

    added auth route

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser, changePassword } = require("../controllers/authController");
+const { registerUser, loginUser, changePassword, checkEmail } = require("../controllers/authController");
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+
+// Check if email exists
+router.post('/check-email', checkEmail);
 
 module.exports = router;
