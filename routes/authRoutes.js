1ef172fb5542392da264a22bb648f9454005ac6d
khavyas <52441771+khavyas@users.noreply.github.com>
2025-10-13 13:12:22 +0000

    test email

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const { registerUser, loginUser, changePassword, checkEmail , sendResetEmail} = require("../controllers/authController");
+const sendEmail = require('../utils/sendEmail');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
 router.post('/change-password', changePassword);
 router.post('/check-email', checkEmail);
 router.post("/send-reset-email", sendResetEmail);
+
+router.get('/test-email', async (req, res) => {
+  try {
+    await sendEmail({
+      to: "khavyasakthi1@gmail.com",
+      subject: "Test Email",
+      html: "<h1>Hello from Gym App!</h1>"
+    });
+    res.json({ success: true, message: "Email sent successfully!" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+});
 
 module.exports = router;
